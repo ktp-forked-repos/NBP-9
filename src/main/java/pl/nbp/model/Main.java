71894684c177084de7e7a1870e1766c0f7813cfb
87kelthuzd <87kelthuzad@gmail.com>
2018-02-17 11:09:18 +0100

    model add

---
+++ src/main/java/pl/nbp/model/Main.java
@@ -0,0 +1,19 @@
+package pl.nbp.model;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public void start(Stage primaryStage) {
+        try {
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
