be18fe7c324eb546c267f2022dce81f7ae5f5c8e
87kelthuzd <87kelthuzad@gmail.com>
2018-02-18 11:39:43 +0100

    add method addUser

--- src/main/java/pl/nbp/model/UserDatabase.java
+++ src/main/java/pl/nbp/model/UserDatabase.java
@@ -1,4 +1,21 @@
 package pl.nbp.model;
 
+import java.util.ArrayList;
+
 public class UserDatabase {
+    static public ArrayList<User> arrayList = new ArrayList<User>();
+    static public User user = null;
+
+    public User addUser() {
+        user = new User.UserBuilder()
+                .id()
+                .firstName()
+                .lastName()
+                .login()
+                .password()
+                .build();
+        return user;
+    }
+
+    
 }
