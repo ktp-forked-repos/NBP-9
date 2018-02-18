e24d1d878807e45f304b82bf0269670afa4ee924
87kelthuzd <87kelthuzad@gmail.com>
2018-02-18 12:11:03 +0100

    final change

--- src/main/java/pl/nbp/model/UserDatabase.java
+++ src/main/java/pl/nbp/model/UserDatabase.java
@@ -6,16 +6,14 @@ public class UserDatabase {
     static public ArrayList<User> arrayList = new ArrayList<User>();
     static public User user = null;
 
-    public User addUser() {
-        user = new User.UserBuilder()
-                .id()
-                .firstName()
-                .lastName()
-                .login()
-                .password()
-                .build();
-        return user;
-    }
-
-
+//    public User addUser() {
+//        user = new User.UserBuilder()
+//                .id()
+//                .firstName()
+//                .lastName()
+//                .login()
+//                .password()
+//                .build();
+//        return user;
+//    }
 }
