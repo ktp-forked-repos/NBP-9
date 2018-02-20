c21718b853e5c20e8de3baac30183fbf5ea981de
87kelthuzad <87kelthuzad@gmail.com>
2018-02-21 00:32:49 +0100

    add tdd

--- src/main/java/pl/nbp/model/Confirm.java
+++ src/main/java/pl/nbp/model/Confirm.java
@@ -8,6 +8,7 @@ public class Confirm {
 
     /**
      * Method to check correct first name and last name and another similar strings
+     *
      * @param string to check
      * @return true if string is correct to regex and return false if is don't correct
      */
@@ -23,15 +24,42 @@ public class Confirm {
 
     /**
      * Method to check correct login
+     *
      * @param string to check
      * @return true if login is correct to regex and return false if is don't correct
      */
     public boolean login(String string) {
         if (string == null || string.isEmpty()) {
-            System.out.printf("\nString is Empty or is a null");
+            System.out.printf("\nLogin is Empty or is a null");
             return false;
-        } else if (string.matches("[A-Za-z][A-Za-z\\S\\d]{7,15}")){
+        } else if (string.matches("[A-Za-z][A-Za-z\\S\\d]{7,15}")) {
             return true;
+        }
+        return false;
+    }
+
+    public boolean password(String password) {
+        if (password == null || password.isEmpty()) {
+            System.out.printf("\nPassword is Empty or is a null");
+            return false;
+        } else if (password.matches("[\\w\\S]{7,15}")) {
+            boolean charDigital = false;
+            boolean charLetter = false;
+            boolean charUpperCase = false;
+            for (int index = 0; index < password.length(); index++) {
+                if (Character.isLetter(password.charAt(index)) && charLetter == false) {
+                    charLetter = true;
+                }
+                if (Character.isDigit(password.charAt(index)) && charDigital == false) {
+                    charDigital = true;
+                }
+                if (Character.isUpperCase(password.charAt(index)) && charUpperCase == false) {
+                    charUpperCase = true;
+                }
+                if (charDigital == true && charLetter == true && charUpperCase == true) {
+                    return true;
+                }
+            }
         }
         return false;
     }
